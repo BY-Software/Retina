a08d2bd4bfa1bb8f77228f9be719b352f8341d89
YucelT94 <yucelterlemezuglu@gmail.com>
2018-01-12 20:18:57 +0300

    Cloud Vision API added

--- app/src/main/java/com/bysoftware/retina/android/activity/MainActivity.java
+++ app/src/main/java/com/bysoftware/retina/android/activity/MainActivity.java
@@ -1,36 +1,75 @@
 package com.bysoftware.retina.android.activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bysoftware.retina.android.BuildConfig;
 import com.bysoftware.retina.android.R;
 import com.bysoftware.retina.android.utility.DoubleClickListener;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.ColorInfo;
+import com.google.api.services.vision.v1.model.DominantColorsAnnotation;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.ImageProperties;
+import com.google.api.services.vision.v1.model.SafeSearchAnnotation;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String API_KEY = BuildConfig.RETINA_API_KEY;
+    private static final String CLOUD_VISION_API_KEY = BuildConfig.RETINA_API_KEY;
+
+    private static final String TAG = "MainActivity";
+    private static final int RECORD_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
 
     private String response;
 
@@ -45,7 +84,25 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text_translate)
     TextView textViewTranslate;
 
-    private String speachText;
+    @BindView(R.id.imageProgress)
+    ProgressBar imageUploadProgress;
+
+    @BindView(R.id.imageView)
+    ImageView imageView;
+
+    @BindView(R.id.spinnerVisionAPI)
+    Spinner spinnerVisionAPI;
+
+    @BindView(R.id.visionAPIData)
+    TextView visionAPIData;
+
+    private Feature feature;
+    private Bitmap bitmap;
+    private String[] visionAPI = new String[]{"LABEL_DETECTION", "LANDMARK_DETECTION", "LOGO_DETECTION", "SAFE_SEARCH_DETECTION", "IMAGE_PROPERTIES"};
+
+    private String api = visionAPI[0];
+
+    private String speechText;
 
     private Unbinder butterKnifeUnbinder;
 
@@ -57,10 +114,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         butterKnifeUnbinder = ButterKnife.bind(this);
 
+        feature = new Feature();
+        feature.setType(visionAPI[0]);
+        feature.setMaxResults(10);
+
+        spinnerVisionAPI.setOnItemSelectedListener(this);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, visionAPI);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerVisionAPI.setAdapter(dataAdapter);
+
         buttonRecord.setOnClickListener(new DoubleClickListener() {
             @Override
             public void onSingleClick(View v) {
-                //TODO: Add Camera
+                takePictureFromCamera();
                 Toast.makeText(getApplicationContext(), "SINGLE CLICK", Toast.LENGTH_SHORT).show();
             }
 
@@ -80,11 +146,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.speech_prompt));
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
@@ -117,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
                                         translate.translate(response,
                                                 Translate.TranslateOption.targetLanguage("en"));
 
-                                speachText = translation.getTranslatedText();
+                                speechText = translation.getTranslatedText();
                                 return null;
                             }
                         }.execute();
@@ -134,10 +198,10 @@ public class MainActivity extends AppCompatActivity {
                                             Locale locale = new Locale("en", "EN");
                                             textToSpeech.setLanguage(locale);
 
-                                            textToSpeech.speak(speachText, TextToSpeech.QUEUE_FLUSH, null);
+                                            textToSpeech.speak(speechText, TextToSpeech.QUEUE_FLUSH, null);
 
-                                            textViewTranslate.setText(speachText);
-                                            Toast.makeText(getApplicationContext(), speachText, Toast.LENGTH_SHORT).show();
+                                            textViewTranslate.setText(speechText);
+                                            Toast.makeText(getApplicationContext(), speechText, Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 });
@@ -148,6 +212,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
         }
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+            callCloudVision(bitmap, feature);
+        }
     }
 
     @Override
@@ -155,5 +224,202 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-}
 
+    public void getSpeech(String speech){
+
+    }
+
+    //Vision
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            buttonRecord.setVisibility(View.VISIBLE);
+        } else {
+            buttonRecord.setVisibility(View.INVISIBLE);
+            makeRequest(Manifest.permission.CAMERA);
+        }
+    }
+
+    private int checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission);
+    }
+
+    private void makeRequest(String permission) {
+        ActivityCompat.requestPermissions(this, new String[]{permission}, RECORD_REQUEST_CODE);
+    }
+
+    public void takePictureFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == RECORD_REQUEST_CODE) {
+            if (grantResults.length == 0 && grantResults[0] == PackageManager.PERMISSION_DENIED
+                    && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                finish();
+            } else {
+                buttonRecord.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void callCloudVision(final Bitmap bitmap, final Feature feature) {
+        imageUploadProgress.setVisibility(View.VISIBLE);
+        final List<Feature> featureList = new ArrayList<>();
+        featureList.add(feature);
+
+        final List<AnnotateImageRequest> annotateImageRequests = new ArrayList<>();
+
+        AnnotateImageRequest annotateImageReq = new AnnotateImageRequest();
+        annotateImageReq.setFeatures(featureList);
+        annotateImageReq.setImage(getImageEncodeImage(bitmap));
+        annotateImageRequests.add(annotateImageReq);
+
+
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    VisionRequestInitializer requestInitializer = new VisionRequestInitializer(CLOUD_VISION_API_KEY);
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(requestInitializer);
+
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(annotateImageRequests);
+
+                    Vision.Images.Annotate annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    return convertResponseToString(response);
+                } catch (GoogleJsonResponseException e) {
+                    Log.d(TAG, "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(TAG, "failed to make API request because of other IOException " + e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                visionAPIData.setText(result);
+                final String[] speechTxtArray = result.split("0");
+                final String speechTxt = speechTxtArray[0];
+                //Response To Speech
+                final Handler textViewHandler = new Handler();
+                textViewHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                            @Override
+                            public void onInit(int status) {
+                                if (status != TextToSpeech.ERROR) {
+                                    Locale locale = new Locale("en", "EN");
+                                    textToSpeech.setLanguage(locale);
+
+                                    textToSpeech.speak(speechTxt, TextToSpeech.QUEUE_FLUSH, null);
+                                }
+                            }
+                        });
+                    }
+                });
+                imageUploadProgress.setVisibility(View.INVISIBLE);
+            }
+        }.execute();
+    }
+
+    @NonNull
+    private Image getImageEncodeImage(Bitmap bitmap) {
+        Image base64EncodedImage = new Image();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        base64EncodedImage.encodeContent(imageBytes);
+        return base64EncodedImage;
+    }
+
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+
+        AnnotateImageResponse imageResponses = response.getResponses().get(0);
+
+        List<EntityAnnotation> entityAnnotations;
+
+        String message = "";
+        switch (api) {
+            case "LANDMARK_DETECTION":
+                entityAnnotations = imageResponses.getLandmarkAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "LOGO_DETECTION":
+                entityAnnotations = imageResponses.getLogoAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "SAFE_SEARCH_DETECTION":
+                SafeSearchAnnotation annotation = imageResponses.getSafeSearchAnnotation();
+                message = getImageAnnotation(annotation);
+                break;
+            case "IMAGE_PROPERTIES":
+                ImageProperties imageProperties = imageResponses.getImagePropertiesAnnotation();
+                message = getImageProperty(imageProperties);
+                break;
+            case "LABEL_DETECTION":
+                entityAnnotations = imageResponses.getLabelAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+        }
+        return message;
+    }
+
+    private String getImageAnnotation(SafeSearchAnnotation annotation) {
+        return String.format("adult: %s\nmedical: %s\nspoofed: %s\nviolence: %s\n",
+                annotation.getAdult(),
+                annotation.getMedical(),
+                annotation.getSpoof(),
+                annotation.getViolence());
+    }
+
+    private String getImageProperty(ImageProperties imageProperties) {
+        String message = "";
+        DominantColorsAnnotation colors = imageProperties.getDominantColors();
+        for (ColorInfo color : colors.getColors()) {
+            message = message + "" + color.getPixelFraction() + " - " + color.getColor().getRed() + " - " + color.getColor().getGreen() + " - " + color.getColor().getBlue();
+            message = message + "\n";
+        }
+        return message;
+    }
+
+    private String formatAnnotation(List<EntityAnnotation> entityAnnotation) {
+        String message = "";
+
+        if (entityAnnotation != null) {
+            for (EntityAnnotation entity : entityAnnotation) {
+                message = message + "    " + entity.getDescription() + " " + entity.getScore();
+                message += "\n";
+            }
+        } else {
+            message = "Nothing Found";
+        }
+        return message;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        api = (String) adapterView.getItemAtPosition(i);
+        feature.setType(api);
+        if (bitmap != null)
+            callCloudVision(bitmap, feature);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
